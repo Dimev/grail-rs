328bf7ec2ccd62487201d38ecd064b24d46b3c55
Dimas <skythedragon@outlook.com>
2021-10-20 12:16:35 +0200

    initial

---
+++ src/lib.rs
@@ -0,0 +1,13 @@
+#![no_std]
+#![forbid(unsafe_code)]
+
+// The main file the synth is in
+// first, define some constants
+
+// Here's how it will work
+// synthesizer iterator to generate sound
+// jitter iterator to add randomness to the frequencies
+// sequencer iterator to blend phonemes
+// intonator to add intonation
+// transcriber to transcribe between text and phoneme
+// parser to parse text and handle potential commands
